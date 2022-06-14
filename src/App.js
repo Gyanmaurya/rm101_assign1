542073844b3d9e38828adbb72e44f315803a0764
Gyanmaurya <gyanprakashme0051@gmail.com>
2022-06-14 19:34:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
-
+import {useState} from 'react'
 function App() {
+ 
+   const [count,setCount]=useState(0)
+    
+   const  addhandle=(value)=>{
+   
+    return setCount(count+value)
+
+   }
+   const removehandel=(value)=>{
+    if(count>0){
+      return setCount(count-value)
+    }
+   
+
+   }
+
+
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{count}</h2>
+      <button data-testid="counter-decrement-button" onClick={()=>addhandle(1)}>+</button>
+      <button data-testid="counter-increment-button" onClick={()=>removehandel(1)}>-</button>
     </div>
   );
 }
